c54406105e69eb1a2ce54277080129faf81552a5
Евгения Полякова <kobra-07@bk.ru>
2023-09-04 06:17:09 +0300

    Add changes PEP8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -14,10 +14,8 @@ class RoleAdminrOrReadOnly(permissions.BasePermission):
 class AuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-            )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
