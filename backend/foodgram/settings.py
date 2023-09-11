743bd8914afaeba68e2955fa71a8c68da0fc16a4
Евгения Полякова <kobra-07@bk.ru>
2023-09-11 12:57:15 +0300

    Delete commit

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-# DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
